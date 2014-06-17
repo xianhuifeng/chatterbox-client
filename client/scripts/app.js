5e80a358da280bf1fa6b35b6b4f008fe489c74bf
Hack Reactor Students <xianhui.feng100@gmail.com>
2014-06-16 21:53:11 -0700

    Added front end for friend adding and chat room creation

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,16 +1,30 @@
   var app = {
     server : 'https://api.parse.com/1/classes/chatterbox',
+
     init : function(){
       $("#submit").on('click', function(e) {
         e.preventDefault();
         app.send(app.getMessage());
       });
+
+      $('#submitRoom').on('click', function(e) {
+        e.preventDefault();
+        app.addRoom($(".newRoom").val());
+      });
+
+      $('#roomSelect').on('click','.roomNumber', function(e){
+        $(this).css('color','orange');
+        $('.selectedRoom').empty();
+        app.clearMessages();
+        $('.selectedRoom').append($(this).text());
+      });
+
       $('#chats').on('click', ".username", function(){
         var context = this;
         app.addFriend(context);
       });
-      app.fetch();
     },
+
     send: function(message) {
       $.ajax({
         url: this.server,
@@ -40,8 +54,7 @@
             return a.createdAt < b.createdAt ?-1:1;
           });
           for(var i = 0; i < data['results'].length; i++) {
-            // console.log(data['results'][i]);
-            app.addMessage(data['results'][i]);
+              app.addMessage(data['results'][i]);
           }
         },
         error: function (data) {
@@ -54,10 +67,12 @@
       var usernameArr = window.location.href.split('username=');
       var username = usernameArr[1];
       var newChat = $('.newChat').val();
+      var newRoom = $('.selectedRoom').text().substr(6);
       var chatObj = {
         username: username,
         text: newChat,
-        roomname: "AppleTree"
+        roomname: newRoom,
+        friendname: undefined
       };
       return chatObj;
     },
@@ -70,23 +85,20 @@
       $('#chats').prepend("<li class='chat'>" + "<span class = 'username'>" +
        userEscape + "</span>" + ":"+ textEscape +"...sent from room: " +
        message.roomname + "</li>");
-      console.log(message.username);
     },
 
     addRoom: function(roomName) {
-      $('#roomSelect').append("<div>" + roomName + "</div>");
+      $('#roomSelect').append("<div class='roomNumber'>" + 'Room: ' + roomName + "</div>");
     },
     addFriend: function(self){
-      $(self).addClass('friend');
-      for()
-    },
-    // getFriendMessages : function(self, message){
-    //   var friendName = $(self).text();
-    //   // console.log($(self));
-
-    // }
+      var friendName = $(self).text();
+      $('.chat:contains(' + friendName + ')').addClass('friend');
+    }
   };
 $(document).ready(function() {
   app.init();
+  setInterval(function(){
+    app.fetch();
+  },3000);
 });
 
